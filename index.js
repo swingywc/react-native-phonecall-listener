ab36a6956d5b2b0091a4de2378e949a34aadf2db
Wing Choy <swingywc@gmail.com>
2020-06-10 16:27:28 +0800

    Fix iOS cannot get EVENT_PHONECALL_RECEIVED variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { RNPhonecallListener } = NativeModules;
 const PhonecallListenerEmitter = new NativeEventEmitter(RNPhonecallListener || {});
 
 const PhonecallListener = {
-  EVENT_PHONECALL_RECEIVED: RNPhonecallListener.EVENT_PHONECALL_RECEIVED,
+  EVENT_PHONECALL_RECEIVED: (RNPhonecallListener && RNPhonecallListener.EVENT_PHONECALL_RECEIVED) || "",
   addEventListener: (name, callback) => PhonecallListenerEmitter.addListener(name, callback),
   onCallReceived: (callback) => {
     return PhonecallListener.addEventListener(PhonecallListener.EVENT_PHONECALL_RECEIVED, callback)
